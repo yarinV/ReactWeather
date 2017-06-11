3a68d397c0dffe3c0fa08dbbea8fa8a072d7440f
yarinV <Yarinva@888holdings.corp>
2017-06-11 10:33:14 +0300

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 
-//Create our app
-var app = express();
+// Create our app
+var   app   = express();
+const PORT  = process.env.PORT || 3000;
+
+app.use(function (req, resp, next) {
+  if(req.headers['x-forwarded-proto'] === 'https') {
+    resp.redirect('http://' + req.hostname + req.url)
+  }
+  else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-    console.log('Express server is up on port 3000');
-});
+app.listen(PORT, function () {
+    console.log("Express loaded at port " + PORT);
+})
